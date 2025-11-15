15663d0f5950db3d41fb48c5c00e1151411f25cf
SAIKAT BISHAL <54259560+saikatbishal@users.noreply.github.com>
2025-11-15 22:59:58 +0530

    deleted unnecessary import statements

--- src/finta/ComingSoon.tsx
+++ src/finta/ComingSoon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ComingSoon = () => (
   <div className="flex flex-col items-center justify-center h-screen">
     <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
